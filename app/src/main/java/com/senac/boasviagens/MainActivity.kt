2246a42bec3ade20efd1f84f6d5db4a619ffb7d0
ARTHUR KLUG JUNIOR <arthur.junior@alunos.sc.senac.br>
2024-03-25 21:45:29 -0300

    Commit inicial

---
+++ app/src/main/java/com/senac/boasviagens/MainActivity.kt
@@ -0,0 +1,200 @@
+package com.senac.boasviagens
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.os.Message
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.senac.boasviagens.ui.theme.BoasViagensTheme
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BoasViagensTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    MyApp()
+                }
+            }
+        }
+    }
+}
+
+
+@Composable
+fun MyApp(){
+
+    var login = remember {
+        mutableStateOf("")
+    }
+
+    var pass = remember {
+        mutableStateOf("")
+    }
+
+    var visibi = remember {
+        mutableStateOf(false)
+    }
+
+    val snackbarHostState = remember {
+        SnackbarHostState()
+    }
+
+    var coroutineScope = rememberCoroutineScope()
+
+    val focus = LocalFocusManager.current
+
+    Scaffold (
+        snackbarHost = {
+            SnackbarHost (snackbarHostState)
+        }
+    ){
+        Column (
+            modifier = Modifier
+                .padding(10.dp)
+                .padding(it)
+
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.viagem),
+                contentDescription = "Cabana",
+                alignment = Alignment.Center,
+                modifier = Modifier
+                    .size(350.dp)
+                    .fillMaxWidth()
+                    .padding(top = 0.dp)
+            )
+
+            Text(
+                text = "Usuário",
+                fontSize = 22.sp
+
+            )
+
+            OutlinedTextField(
+                value = login.value,
+                onValueChange = {login.value = it},
+                label = {
+                    Text(text = "Login")
+                },
+                modifier = Modifier
+                    .padding(top = 15.dp)
+                    .fillMaxWidth()
+            )
+
+            Text(
+                text = "Senha",
+                fontSize = 22.sp,
+                modifier = Modifier
+                    .padding(top = 15.dp)
+
+            )
+
+            OutlinedTextField(
+                value = pass.value,
+                onValueChange = {pass.value = it},
+                label = {
+                    Text(text = "Password")
+                },
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Password
+                ),
+                visualTransformation =
+                if(visibi.value)
+                    VisualTransformation.None
+                else
+                    PasswordVisualTransformation(),
+
+                trailingIcon = {
+                    IconButton(onClick = {
+                        visibi.value = !visibi.value
+                    }) {
+                        if(visibi.value)
+                            Icon(
+                                painterResource(id = R.drawable.visiblee), "")
+                        else
+                            Icon(
+                                painterResource(id = R.drawable.nonvisible), "")
+                    }
+                },
+                modifier = Modifier
+                    .padding(top = 15.dp)
+                    .fillMaxWidth()
+            )
+
+            Button(
+                onClick = {
+                    if (pass.value == "senha" && login.value == "Arthur")
+                        coroutineScope.launch {
+                            focus.clearFocus()
+                            snackbarHostState.showSnackbar(
+                                message = "login executado",
+                                withDismissAction = true
+                            )
+                        }else{
+                        coroutineScope.launch {
+                            focus.clearFocus()
+                            snackbarHostState.showSnackbar(
+                                message = "login ou Senha errados",
+                                withDismissAction = true
+                            )
+                        }
+                        }
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 15.dp)
+            ) {
+                Text(
+                    text = "Login",
+                    fontSize = 22.sp
+                )
+            }
+
+
+        }
+    }
+
+}
+
+fun Context.toast(message: CharSequence) =
+    Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+
